0ba79be3d67d1791103bf1aad9a5094c02c6d112
Antônio Augusto <antonioabritto@gmail.com>
2018-01-31 01:09:56 -0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class ContactList extends Component {
+export default class App extends Component {
   render() {
-    const people = this.props.contacts
     return (
-      <ol>
-        { people.map((person, index) => (
-          <li key={index}>{person.name}</li>
-        )) }
-      </ol>
+      <div></div>
     );
   }
 }
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <ContactList contacts={[
-          {name: 'Joao'},
-          {name: 'Maria'},
-          {name: 'Jose'}
-        ]}/>
-        <ContactList contacts={[
-          {name: 'John'},
-          {name: 'Mary'},
-          {name: 'Joseph'}
-        ]}/>
-      </div>
-    );
-  }
-}
-
-export default App;
