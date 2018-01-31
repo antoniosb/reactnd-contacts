50fc3c8997bc3e543bd7b133b71332bd16a56f54
Antônio Augusto <antonioabritto@gmail.com>
2018-01-31 01:35:01 -0200

    creating ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts.js'
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 export default class App extends Component {
   render() {
     return (
-      <div>Foo</div>
+      <div>
+        <ListContacts contacts={contacts} />
+      </div>
     );
   }
 }
---
+++ src/ListContacts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ListContacts extends Component {
+  render() {
+    console.log('Props', this.props)
+    return (
+      <ol className='contact-list'>
+
+      </ol>
+    );
+  }
+}
