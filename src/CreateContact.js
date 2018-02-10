8528e92d7e6e84d72c10c1dc5b844f9f6007efd1
Antônio Augusto <antonioabritto@gmail.com>
2018-02-10 21:56:39 -0200

    add inputs to the create contact form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput.js'
 
 export default class CreateContact extends Component {
   render() {
     return (
-      <div>Create a Contact!</div>
+      <div>
+        <Link className='close-create-contact' to='/'>Close</Link>
+        <form className='create-contact-form'>
+          <ImageInput
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name='name' placeholder='Name'/>
+            <input type="email" name='email' placeholder='Email'/>
+            <button type='submit'>Add contact</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
