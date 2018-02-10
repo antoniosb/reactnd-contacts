380a71accf2536030e9949b142d76b4572d6f3c6
Antônio Augusto <antonioabritto@gmail.com>
2018-02-10 20:40:30 -0200

    adding api call from within an event lyfeclycle hook

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts.js';
+import * as ContatcsAPI from './utils/ContactsAPI.js'
 
 export default class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'ryan',
-        name: 'Ryan Florence',
-        email: 'ryan@reacttraining.com',
-        avatarURL: 'http://localhost:5001/ryan.jpg',
-      },
-      {
-        id: 'michael',
-        name: 'Michael Jackson',
-        email: 'michael@reacttraining.com',
-        avatarURL: 'http://localhost:5001/michael.jpg',
-      },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        email: 'tyler@reacttraining.com',
-        avatarURL: 'http://localhost:5001/tyler.jpg',
-      },
-    ]
+    contacts: []
   }
+
+  componentDidMount() {
+    ContatcsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
+
   removeContact = (contact) => {
     this.setState((prevState) => ({
       contacts: prevState.contacts.filter((c) => c.id !== contact.id)
